9500b85e84d8855fedfebaf486861d95cd272fb8
padmarani10 <143779038+padmarani10@users.noreply.github.com>
2023-09-20 21:09:18 +0530

    #include<stdio.h> int main() {     int n; scanf("%d",&n); for(int i=1;i<=n;i++) {     for(int j=1;j<=n-2;j++) {     printf("%d",j); } for(int k=1;k<n-2;k++) {     printf("%d",k); } printf(" "); }
    }

---
+++ print_the_following_pattern_7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+    for(int j=1;j<=n-2;j++)
+{
+    printf("%d",j);
+}
+for(int k=1;k<n-2;k++)
+{
+    printf("%d",k);  
+}
+printf("
+");
+}
+    
+}

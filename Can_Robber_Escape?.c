08a45383165761ea4a579d82eaf2e76517d0a9d9
padmarani10 <143779038+padmarani10@users.noreply.github.com>
2023-10-20 10:50:28 +0530

    #include<stdio.h> int main() {     int n,s=0;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]%2==1)         {             s++;         }     }     if(s>2)     {         printf("NO");     }     else     {         printf("YES");     } }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,odd=0;
-scanf("%d",&n);  
-int arr[n];   
-for(int i=0;i<n;i++)    
-{
-    scanf("%d",&arr[i]);        
-if(arr[i]%2==1) 
-odd++;
-}
-if(odd>2)
-printf("NO"); 
-else    
-printf("YES");
-
-    
+    int n,s=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]%2==1)
+        {
+            s++;
+        }
+    }
+    if(s>2)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
 }

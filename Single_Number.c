0cf755a1eab3dd3f96dd82f1246626c13ac16acd
padmarani10 <143779038+padmarani10@users.noreply.github.com>
2023-10-20 11:16:03 +0530

    #include <stdio.h>
    int findSingleNumber(int arr[], int n) {
        int result = 0;
        for (int i = 0; i < n; i++) {
            result ^= arr[i];
        }
        return result;
    }
    
    int main() {
        int n;
        scanf("%d", &n);
        int arr[n];
    
        for (int i = 0; i < n; i++) {
            scanf("%d", &arr[i]);
        }
    
        int singleNumber = findSingleNumber(arr, n);
    
        printf("%d
    ", singleNumber);
    
        return 0;
    }

---
+++ Single_Number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int findSingleNumber(int arr[], int n) {
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        result ^= arr[i];
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int singleNumber = findSingleNumber(arr, n);
+
+    printf("%d
+", singleNumber);
+
+    return 0;
+}
